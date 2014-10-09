48c7db3f5ed99b3b2c8ffc945f01c44fc4e19700
usuario <usuario@10.22.143.8>
2014-10-09 11:53:31 +0100

    kata 2 con anaplacido

---
+++ src/kata2/Kata2.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author usuario
+ */
+public class Kata2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Map<Integer, Integer> histogram = new HashMap<>();
+        int[] numbers = {2, 2, 11, 4, 2, 6, 11, 2, 3, 5, 11, 5};
+        
+        for (int number : numbers) {
+            histogram.put(number, getFrequency(number, histogram)+1);
+        }
+        for (Integer key : histogram.keySet()) {
+            System.out.println(key + "-> " + histogram.get(key));
+        }
+        
+        int number = 0;
+        int maxOcurrence=0;
+        
+        for (Integer key : histogram.keySet()) {
+            if(histogram.get(key)>maxOcurrence){
+                number = key;
+                maxOcurrence = histogram.get(key);
+            }
+            
+        }
+        
+        System.out.println("El numero que mas se repite es: " + number + "->" + maxOcurrence);
+        
+        
+        
+        
+    }
+
+    private static Integer getFrequency(int number, Map<Integer, Integer> histogram) {
+        if(histogram.containsKey(number)){
+            return histogram.get(number);
+        }
+        return 0;
+    }
+}
